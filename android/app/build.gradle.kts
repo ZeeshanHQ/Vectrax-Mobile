2269e479be7223ee78b2dc3cea85c21e85b79787
ZeeshanHQ <huaweigps1322@gmail.com>
2026-08-09 23:36:44 +0500

    fix: restored ndkVersion to 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.astraventa.vectrax"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "25.1.8937393"
+    ndkVersion = "27.0.12077973"
 
     lint {
         checkReleaseBuilds = false
